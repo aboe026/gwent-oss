2f1bafbb9f3a20e0a49122e47f4d15551effd8af
Adam Boe <adamboe@outlook.com>
2023-11-14 14:41:27 -0800

    Fix watch command

---
+++ svcs/api/scripts/wait-for-initial-build.ts
@@ -0,0 +1,23 @@
+import fs from 'fs-extra'
+import path from 'path'
+
+import { sleep } from '@gwent/utils'
+
+//
+;(async () => {
+  try {
+    const timeoutSeconds = 30
+    let builtExists = false
+    const start = Date.now()
+    while (!builtExists && (Date.now() - start) / 1000 < timeoutSeconds) {
+      await sleep(1)
+      builtExists = await fs.exists(path.join(__dirname, '../build/src/index.js'))
+    }
+    if (!builtExists) {
+      throw Error(`App not successfully built after "${timeoutSeconds}" seconds`)
+    }
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+})()
