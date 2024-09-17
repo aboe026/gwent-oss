868a2cd16c73f1efceffcb56f420db4c829865cc
Adam Boe <adamboe@outlook.com>
2024-09-17 16:58:04 -0700

    fix unit tests for CI

--- README.md
+++ README.md
@@ -287,6 +287,10 @@ A list of things to be done in the future:
 - Introspect GraphQL queries/mutations to determine which fields to project/return from DB
 - Have api and ui use same Dockerfile (just with different build args)
 - Fold [deck-filter.ts](libs\graphql-schema\src\deck-filter.ts) into normal GraphQL schema (add sort and filter fields to units query)
+- Fix root "yarn build"
+  - right now it runs "yarn build" in all workspaces simultaneously.
+  - This is a problem because there are explicit dependencies on libraries being built first
+  - Need to have a script generate dependency tree, then perform builds in reverse order
 
 ### External Bugs
 
--- libs/utils/test/unit/format-date.unit.test.ts
+++ libs/utils/test/unit/format-date.unit.test.ts
@@ -10,7 +10,23 @@ describe('format-date', () => {
   describe('formatTime', () => {
     it('returns human readable time from iso string', () => {
       const iso = '2024-09-03T21:25:02.835Z'
+      // needed to stub/spy out this method because otherwise
+      // this test fails in CI due to (seemingly) timezone differences
+      // (i.e. it fails with "9:25 PM" instead of "2:25 PM")
+      const toLocaleTimeStringSpy = jest.spyOn(Date.prototype, 'toLocaleTimeString').mockReturnValue('2:25 PM')
+
       expect(formatTime(iso)).toEqual('2:25 PM')
+
+      expect(toLocaleTimeStringSpy.mock.calls).toEqual([
+        [
+          'en-us',
+          {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          },
+        ],
+      ])
     })
   })
 })
