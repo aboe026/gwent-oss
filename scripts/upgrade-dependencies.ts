db01777d6c091ea440cf553a5a22efe746b43e1e
Adam Boe <adamboe@outlook.com>
2024-09-20 16:35:31 -0700

    Upgrade dependencies

--- scripts/upgrade-dependencies.ts
+++ scripts/upgrade-dependencies.ts
@@ -5,11 +5,13 @@ import { replaceInFile } from 'replace-in-file'
 import execa from './execute-async'
 
 const DENY_LIST: string[] = [
-  'open', // 9.0 switched to ESM, need to switch to ESM to be able to use
-  'url-join', // 5.0 switched to ESM, need to switch to ESM to be able to use
-  'prettier', // 3.0 broke auto pretty on save (TypeError: Invalid host defined options)
-  'eslint-plugin-prettier', // 5.0 breaks if using 2.0 of prettier (TypeError: prettier.resolveConfig is not a function)
   'eslint', // 9.0 breaks with TypeError: [ERR_IMPORT_ASSERTION_TYPE_MISSING]: Module "file:///.eslintrc.json?mtime=1714512618315" needs an import assertion of type "json"
+  'eslint-plugin-prettier', // 5.0 breaks if using 2.0 of prettier (TypeError: prettier.resolveConfig is not a function)
+  'graphql-request', // 7.0.0 switched to ESM, need to switch to ESM to be able to use
+  'open', // 9.0 switched to ESM, need to switch to ESM to be able to use
+  'prettier', // 3.0 broke auto pretty on save (TypeError: Invalid host defined options)
+  'replace-in-file', // 8.0.0 switched to ESM, need to switch to ESM to be able to use
+  'url-join', // 5.0 switched to ESM, need to switch to ESM to be able to use
 ]
 
 //
