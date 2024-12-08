f13660564a7fa044ec06469ed04f3c5aac0d5afa
Adam Boe <adamboe@outlook.com>
2024-12-08 12:28:01 -0800

    Full unit test coverage

--- svcs/api/test/unit/factions-query.unit.test.ts
+++ svcs/api/test/unit/factions-query.unit.test.ts
@@ -2,13 +2,23 @@ import { Context } from '@gwent/graphql-schema/context'
 import FactionResolver from '../../src/graphql/resolvers/types/faction-resolver'
 import FactionsQuery from '../../src/graphql/resolvers/queries/factions-query'
 import FactionStore from '../../src/database/stores/faction-store'
-import { QueryFactionsArgs } from '@gwent/graphql-schema/resolver-typings'
+import { FactionKey, QueryFactionsArgs } from '@gwent/graphql-schema/resolver-typings'
 import TestUtil from '../test-util'
 
 describe('factions-query', () => {
   describe('factions', () => {
-    it('reaches out to FactionStore', async () => {
+    it('reaches out to FactionStore without keys arg', async () => {
       await testFactions({})
+    })
+    it('reaches out to FactionStore with single keys arg', async () => {
+      await testFactions({
+        keys: [FactionKey.Neutral],
+      })
+    })
+    it('reaches out to FactionStore with multiple keys arg', async () => {
+      await testFactions({
+        keys: [FactionKey.NorthernRealms, FactionKey.Neutral],
+      })
     })
     it('logs to trace if enabled', async () => {
       await testFactions({
@@ -18,14 +28,16 @@ describe('factions-query', () => {
   })
 })
 
-async function testFactions({ traceEnabled }: { traceEnabled?: boolean }) {
+async function testFactions({ keys, traceEnabled }: { keys?: FactionKey[]; traceEnabled?: boolean }) {
   const context: Context = {
     session: {
       user: TestUtil.getDbUser({}),
     },
   }
-  // TODO: test with keys
   const args: QueryFactionsArgs = {}
+  if (keys) {
+    args.keys = keys
+  }
   const logPrefix = `factions by "${context.session?.user?._id}"`
   const faction = TestUtil.getDbFaction({})
   const resolvedFaction = TestUtil.getFactionFromDbFaction(faction)
@@ -39,7 +51,7 @@ async function testFactions({ traceEnabled }: { traceEnabled?: boolean }) {
 
   await expect(FactionsQuery.factions(args, context, null as any)).resolves.toEqual([resolvedFaction])
 
-  expect(getSpy.mock.calls).toEqual([[{}]])
+  expect(getSpy.mock.calls).toEqual([[{ keys }]])
   expect(factionResolverSpy.mock.calls).toEqual([
     [
       {
