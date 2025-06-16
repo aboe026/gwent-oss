2a81758e1e6cd7b03133faca4a1dfadef198ccb2
Adam Boe <adamboe@outlook.com>
2025-06-15 21:39:02 -0700

    Unit tests

--- libs/utils/src/get-no-impact-message.ts
+++ libs/utils/src/get-no-impact-message.ts
@@ -1,25 +1,24 @@
 import { EffectKey } from '@gwent/graphql-schema/resolver-typings'
 
 export default function getNoImpactMessage({ effectKey }: { effectKey: EffectKey }): string {
-  let noImpactMessage = ''
   if (effectKey === EffectKey.Bond) {
-    noImpactMessage = 'No similar units in row to bond with.'
+    return 'No similar units in row to bond with.'
   } else if (effectKey === EffectKey.Horn) {
-    noImpactMessage = 'No eligible units in row to strengthen.'
+    return 'No eligible units in row to strengthen.'
   } else if (effectKey === EffectKey.Mardroeme) {
-    noImpactMessage = 'No Berserkers in row to transform.'
+    return 'No Berserkers in row to transform.'
   } else if (effectKey === EffectKey.Medic) {
-    noImpactMessage = 'No eligible units in Lost to revive.'
+    return 'No eligible units in Lost to revive.'
   } else if (effectKey === EffectKey.Muster) {
-    noImpactMessage = 'No eligible units in Draw to muster.'
+    return 'No eligible units in Draw to muster.'
   } else if (effectKey === EffectKey.Morale) {
-    noImpactMessage = 'No eligible units in row to strengthen.'
+    return 'No eligible units in row to strengthen.'
   } else if (effectKey === EffectKey.Scorch) {
-    noImpactMessage = 'No eligible units on battlefield to scorch.'
+    return 'No eligible units on battlefield to scorch.'
   } else if (effectKey === EffectKey.Spy) {
-    noImpactMessage = 'No units in Draw to add to Hand.'
+    return 'No eligible units in Draw to add to Hand.'
   } else if (effectKey === EffectKey.Weather) {
-    noImpactMessage = 'No eligible units on battlefield to weaken.'
+    return 'No eligible units on battlefield to weaken.'
   }
-  return noImpactMessage
+  throw Error(`Effect "${effectKey}" is not impactable.`)
 }
--- libs/utils/test/unit/sort.unit.test.ts
+++ libs/utils/test/unit/sort.unit.test.ts
@@ -1,4 +1,4 @@
-import sortObjectArray, { getNestedProperty, getSortOrder } from '../../src/sort'
+import sortObjectArray, { getSortOrder } from '../../src/sort'
 
 describe('sort', () => {
   describe('sortObjectArray', () => {
@@ -1206,60 +1206,6 @@ describe('sort', () => {
           })
         })
       })
-    })
-  })
-  describe('getNestedProperty', () => {
-    it('returns undefined if undefined', () => {
-      expect(
-        getNestedProperty({
-          nestedProperty: 'foo',
-          obj: undefined,
-        })
-      ).toEqual(undefined)
-    })
-    it('returns undefined if property does not exist', () => {
-      expect(
-        getNestedProperty({
-          nestedProperty: 'foo',
-          obj: {},
-        })
-      ).toEqual(undefined)
-    })
-    it('returns root level property', () => {
-      expect(
-        getNestedProperty({
-          nestedProperty: 'foo',
-          obj: {
-            foo: 'bar',
-          },
-        })
-      ).toEqual('bar')
-    })
-    it('returns nested property', () => {
-      expect(
-        getNestedProperty({
-          nestedProperty: 'foo.bar',
-          obj: {
-            foo: {
-              bar: 1,
-            },
-          },
-        })
-      ).toEqual(1)
-    })
-    it('returns deeply nested property', () => {
-      expect(
-        getNestedProperty({
-          nestedProperty: 'foo.bar.biz',
-          obj: {
-            foo: {
-              bar: {
-                biz: 1,
-              },
-            },
-          },
-        })
-      ).toEqual(1)
     })
   })
   describe('getSortOrder', () => {
