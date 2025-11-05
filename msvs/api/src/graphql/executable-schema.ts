115899f72420f8dbc4ac2b807909e0014f78b3a7
Adam Boe <adamboe@outlook.com>
2025-11-04 18:45:39 -0800

    Remove graphql-shield

--- msvs/api/src/graphql/executable-schema.ts
+++ msvs/api/src/graphql/executable-schema.ts
@@ -1,17 +1,12 @@
-import { applyMiddleware } from 'graphql-middleware'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-import Permissions from './permissions'
 import resolvers from './resolvers/resolvers'
 import { typeDefs } from '@gwent/graphql-schema'
 
 /**
  * The executable GraphQL schema.
  */
-export default applyMiddleware(
-  makeExecutableSchema({
-    typeDefs,
-    resolvers,
-  }),
-  Permissions.shield()
-)
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
