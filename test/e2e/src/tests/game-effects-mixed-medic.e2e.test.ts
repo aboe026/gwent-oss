b6437c2115efbc9ba7f8c4c252319ffb0ae800c1
Adam Boe <adamboe@outlook.com>
2026-06-05 12:42:07 -0700

    E2E tests

--- test/e2e/src/tests/game-effects-mixed-medic.e2e.test.ts
+++ test/e2e/src/tests/game-effects-mixed-medic.e2e.test.ts
@@ -1,7 +1,6 @@
-import { Combat, EffectKey, FactionKey } from '@gwent/node-client'
+import { Combat, FactionKey } from '@gwent/node-client'
 import createGameManager from '../util/game-manager'
 import { E2eCtx, getFixtureCtx, getTestCtx, getScenario } from '../util/e2e-ctx'
-import GamePage from '../page-objects/game-page'
 
 const fixture = getFixtureCtx<E2eCtx, E2eCtx>()
 const test = getTestCtx<E2eCtx, E2eCtx>()
@@ -31,50 +30,10 @@ test('Agile unit can be revived to same combat row', async (t) => {
     unitName: unitName2,
     medicing: true,
   })
-
-  await GamePage.toggleImpacts({
-    unitName: unitName2,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName1,
     eligibleCombats: [Combat.Close, Combat.Ranged],
     revivedBy: unitName2,
-  })
-
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName1,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
   })
 })
 
@@ -101,51 +60,11 @@ test('Agile unit can be revived to different combat row', async (t) => {
     unitName: unitName2,
     medicing: true,
   })
-
-  await GamePage.toggleImpacts({
-    unitName: unitName2,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName1,
     combat: Combat.Ranged,
     eligibleCombats: [Combat.Close, Combat.Ranged],
     revivedBy: unitName2,
-  })
-
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName1,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
   })
 })
 
@@ -197,47 +116,9 @@ test('Avenging units can be revived', async (t) => {
     unitName: unitName4,
     medicing: true,
   })
-  await GamePage.toggleImpacts({
-    unitName: unitName4,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName4,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName2,
     revivedBy: unitName4,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName4,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName2,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
   })
 })
 
@@ -272,84 +153,24 @@ test('Bonded units can rebond after revival', async (t) => {
   await gameManager.pass({
     switchTurnsWith: gameManager.self.gamePlayer,
   })
-  await gameManager.initialize({})
 
   await gameManager.deploy({
     unitName: unitName2,
     medicing: true,
   })
-  await GamePage.toggleImpacts({
-    unitName: unitName2,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName1,
     bonding: [],
     revivedBy: unitName2,
   })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName2,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName1,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
 
   await gameManager.pass({})
+  await gameManager.initialize({})
 
   await gameManager.deploy({
     unitName: unitName3,
     medicing: true,
   })
-  await GamePage.toggleImpacts({
-    unitName: unitName3,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName3,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName1,
     effectiveStrength: 8,
@@ -363,21 +184,102 @@ test('Bonded units can rebond after revival', async (t) => {
     ],
     revivedBy: unitName3,
   })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName3,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName1,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
+})
+
+test('Revived unit can be decoyed', async (t) => {
+  const unitName1 = 'Ves'
+  const unitName2 = 'Dun Banner Medic'
+  const unitName3 = 'Decoy'
+  const gameManager = await createGameManager({
+    label: `${getScenario(t)}-${t.ctx.start}`,
+    self: {
+      faction: FactionKey.NorthernRealms,
+      handUnitNames: [unitName1, unitName2, unitName3],
+    },
+  })
+  await gameManager.deploy({
+    unitName: unitName1,
+  })
+  await gameManager.pass({})
+  await gameManager.pass({
+    switchTurnsWith: gameManager.self.gamePlayer,
+  })
+  await gameManager.deploy({
+    unitName: unitName2,
+    medicing: true,
+  })
+  await gameManager.deploy({
+    unitName: unitName1,
+    revivedBy: unitName2,
+  })
+  await gameManager.pass({})
+  await gameManager.initialize({})
+
+  await gameManager.deploy({
+    unitName: unitName3,
+    decoying: {
+      name: unitName1,
+      effectiveStrength: 5,
+      player: gameManager.self.gamePlayer,
+      row: Combat.Close,
+    },
+  })
+})
+
+test('Decoyed medic can revive another unit', async (t) => {
+  const unitName1 = 'Ves'
+  const unitName2 = 'Vesemir'
+  const unitName3 = 'Dun Banner Medic'
+  const unitName4 = 'Decoy'
+
+  const gameManager = await createGameManager({
+    label: `${getScenario(t)}-${t.ctx.start}`,
+    self: {
+      faction: FactionKey.NorthernRealms,
+      handUnitNames: [unitName1, unitName2, unitName3, unitName4],
+    },
+  })
+  await gameManager.deploy({
+    unitName: unitName1,
+  })
+  await gameManager.pass({})
+  await gameManager.deploy({
+    unitName: unitName2,
+  })
+  await gameManager.pass({
+    switchTurnsWith: gameManager.self.gamePlayer,
+  })
+  await gameManager.deploy({
+    unitName: unitName3,
+    combat: Combat.Siege,
+    medicing: true,
+  })
+  await gameManager.deploy({
+    unitName: unitName1,
+    revivedBy: unitName3,
+  })
+  await gameManager.pass({})
+
+  await gameManager.deploy({
+    unitName: unitName4,
+    combat: Combat.Siege,
+    decoying: {
+      name: unitName3,
+      effectiveStrength: 5,
+      player: gameManager.self.gamePlayer,
+      row: Combat.Siege,
+    },
+  })
+  await gameManager.initialize({})
+
+  await gameManager.deploy({
+    unitName: unitName3,
+    combat: Combat.Siege,
+    medicing: true,
+  })
+  await gameManager.deploy({
+    unitName: unitName2,
+    revivedBy: unitName3,
   })
 })
 
@@ -411,84 +313,24 @@ test('Horned units can rehorn after revival', async (t) => {
   await gameManager.pass({
     switchTurnsWith: gameManager.self.gamePlayer,
   })
-  await gameManager.initialize({})
 
   await gameManager.deploy({
     unitName: unitName3,
     combat: Combat.Ranged,
     medicing: true,
   })
-  await GamePage.toggleImpacts({
-    unitName: unitName3,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName3,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName1,
     revivedBy: unitName3,
   })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName3,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName1,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
 
   await gameManager.pass({})
+  await gameManager.initialize({})
 
   await gameManager.deploy({
     unitName: unitName4,
     medicing: true,
   })
-  await GamePage.toggleImpacts({
-    unitName: unitName4,
-    round: gameManager.round,
-    userName: gameManager.self.gamePlayer.name,
-  })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName4,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: 'Choosing...',
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
-
   await gameManager.deploy({
     unitName: unitName2,
     revivedBy: unitName4,
@@ -507,22 +349,4 @@ test('Horned units can rehorn after revival', async (t) => {
       },
     ],
   })
-  await GamePage.verifyImpacts({
-    moves: [
-      {
-        unitName: unitName4,
-        round: gameManager.round,
-        userName: gameManager.self.gamePlayer.name,
-        effectKey: EffectKey.Medic,
-        impacts: [
-          {
-            unitName: unitName2,
-            username: gameManager.self.gamePlayer.name,
-          },
-        ],
-      },
-    ],
-  })
 })
-
-// TODO: decoyed medic can revive another unit
